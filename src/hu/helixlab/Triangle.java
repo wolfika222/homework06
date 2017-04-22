5f8a59ebf60e6f20d0e1b8ed7983fe133f42d384
wolfika222 <farkas0528@gmail.com>
2017-04-22 20:33:13 +0200

    #2

--- src/hu/helixlab/Triangle.java
+++ src/hu/helixlab/Triangle.java
@@ -22,7 +22,7 @@ public class Triangle extends AbstractShape {
     } // már kész
 
     public float getPerimeter() {
-        return (a * beta) / 2;
+        return (float) ((a * c * Math.sin(beta)) / 2);
     } // már kész
 
     public float getA() {
